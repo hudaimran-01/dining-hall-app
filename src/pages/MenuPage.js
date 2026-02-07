2a260ba16902b03e0127c0a5e44cd69602f67d28
Birwa Tahir <btahir@brynmawr.edu>
2026-02-06 22:56:10 -0500

    resolved merge conflicts, created missing pages

---
+++ src/pages/MenuPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MenuPage() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Menu Page</h1>
+      <p>Menu content coming soon.</p>
+    </div>
+  );
+}
+
+export default MenuPage;
